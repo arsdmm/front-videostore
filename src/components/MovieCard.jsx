5a51b5a0f6d90c44454a29addeed8c8b200f2d33
Dmytro Litvinov <dmlitvinov99@gmail.com>
2025-08-06 15:19:50 -0400

    Images

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './MovieCard.css';
 
-const MovieCard = ({ id, title, poster }) => {
+const MovieCard = ({ id, title, imageUrl }) => {
   return (
     <Link to={`/movies/${id}`} className="movie-card">
       <div className="card">
-        <img src={poster} alt={title} />
+        <img src={imageUrl} alt={title} />
         <h3>{title}</h3>
       </div>
     </Link>
